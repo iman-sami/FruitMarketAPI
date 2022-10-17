4c2f052fb1fb9b73e893cf4f18e05f41e458ec3f
iman-sami <imansamifaith@gmail.com>
2022-10-17 20:26:05 +0300

    auth controller added

---
+++ controller/auth.js
@@ -0,0 +1,15 @@
+import { catchAsync } from "../middleware/errorHandlings"
+
+
+export const AuthController = {
+ Login: catchAsync(async (req,res,next)=>{
+ 
+
+
+ }),
+ Register: catchAsync(async (Req,res,next)=>{
+
+ }),
+
+
+}
