c76dfa83bb19bf8e858392d1edb9fa09e4dcf1f1
iman-sami <imansamifaith@gmail.com>
2022-10-17 20:51:22 +0300

    app.js express updated

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import { PORT } from './config/constant.js';
-
+import  DbConnection from './config/Db.js';
 const app = express()
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded())
+
+DbConnection.connect((err)=>{
+    if (err) return console.log(err.message)
+
+    console.log("Connected To DB")
+})
 app.listen(PORT,()=>{
     console.log(`Connected to Port ${PORT}`)
 })
--- controller/auth.js
+++ controller/auth.js
@@ -1,13 +1,36 @@
-import { catchAsync } from "../middleware/errorHandlings"
+import  DbConnection  from "../config/Db.js"
+import bcrypt from 'bcryptjs'
+import { serverError } from "../config/serverError.js"
+import { catchAsync } from "../middleware/errorHandlings.js"
+import { AddUser } from "../query/user.js"
 
 
 export const AuthController = {
- Login: catchAsync(async (req,res,next)=>{
- 
+ Register: catchAsync(async (req,res,next)=>{
+    const data = req.body
+    const check =`select * from user where email='${data.email}'`
+     data.password = bcrypt.hash(data.password,12)
+    DbConnection.query(check,(err,results)=>{
 
+        if (err) return next (new serverError(err.message,404));
+        if(results>0){
+             res.json({
+                message:"User Already Exsist!!!"
+            })
+        }
+        DbConnection.query(AddUser(data),(err,results)=>{
+
+            if(err) return next (new serverError(err.message,404))
+            res.json({
+                message:"User Registered Succesfully",
+                statusCode:results.insertId
+            })
+        })
+
+    })
 
  }),
- Register: catchAsync(async (Req,res,next)=>{
+ Login: catchAsync(async (Req,res,next)=>{
 
  }),
 
