0be8165da4abd675b077c9a508fa4391d2d17719
iman-sami <imansamifaith@gmail.com>
2022-11-07 21:57:49 +0300

    auth controller updated

--- controller/auth.js
+++ controller/auth.js
@@ -2,37 +2,47 @@ import  DbConnection  from "../config/Db.js"
 import bcrypt from 'bcryptjs'
 import { serverError } from "../config/serverError.js"
 import { catchAsync } from "../middleware/errorHandlings.js"
-import { AddUser } from "../query/user.js"
+import { AddUser,getuser } from "../query/user.js"
+import { createToken } from "../config/utils.js"
 
 
 export const AuthController = {
- Register: catchAsync(async (req,res,next)=>{
+ register: catchAsync(async (req,res,next)=>{
     const data = req.body
-    const check =`select * from user where email='${data.email}'`
-     data.password = bcrypt.hash(data.password,12)
-    DbConnection.query(check,(err,results)=>{
-
-        if (err) return next (new serverError(err.message,404));
-        if(results>0){
-             res.json({
-                message:"User Already Exsist!!!"
-            })
+    const checkData = `select * from user where email = '${data.email};'`
+    data.password = await bcrypt.hash(data.password,12)
+    DbConnection.query(checkData,(err,result)=>{
+        if(err) return next (new serverError(err.message,404))
+        if(result>0){
+            return next(new serverError("User Already Exsist ",404))
         }
-        DbConnection.query(AddUser(data),(err,results)=>{
-
-            if(err) return next (new serverError(err.message,404))
-            res.json({
-                message:"User Registered Succesfully",
-                statusCode:results.insertId
-            })
+      
+        DbConnection.query(AddUser(),data,(err,results)=>{
+            if(err) return new (serverError(err.message,404))
+            const users = results[0]
+            createToken(users,results,res)
         })
-
     })
 
  }),
- Login: catchAsync(async (Req,res,next)=>{
 
- }),
+ // login
 
+ login: catchAsync(async (req,res,next)=>{
+   
+    const data = req.body
+    const checkuser = `select * from user where email = '${data.email};'`
+
+    DbConnection.query(checkuser,async (err,results)=>{
+        if(err) return next(new serverError(err.message,404))
+        if(results == 0 ) return next (new serverError("User not Registered!"))
+
+         let correctpass = await bcrypt.compare(data.password,results[0].password)
+        if(!correctpass) return next ("password doesn't match",404)
+        createToken(results[0],results,res)
+    })
+
+
+ })
 
 }
